e3d78d7e0014786bc8831b6b9b02002ec350338e
olinssar <143740876+olinssar@users.noreply.github.com>
2023-09-16 00:48:35 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,45 +1,64 @@
 import re
 import sys
 from os import getenv
+
 from dotenv import load_dotenv
 from pyrogram import filters
+
 load_dotenv()
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH")
+
+API_ID = int(getenv("API_ID", "25710552"))
+API_HASH = getenv("API_HASH", "1f08de6bf83142a21f8601dd08d2a84c")
+
 BOT_TOKEN = getenv("BOT_TOKEN")
+
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", "900"))
+
 SONG_DOWNLOAD_DURATION = int(getenv("SONG_DOWNLOAD_DURATION_LIMIT", "180"))
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001657259154"))
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "ǫᴜʀᴀɴ ʟɪᴠᴇ")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "5422153027").split()))
-HEROKU_API_KEY = getenv("HEROKU_API_KEY")
-BOT_ID = getenv("BOT_ID")
+
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID",""))
+
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME","⁪⁬ǫᴜʀᴀɴ ʟɪᴠᴇ")
+OWNER_ID = list(map(int, getenv("OWNER_ID","5422153027").split()))
+HEROKU_API_KEY = getenv("HEROKU_API_KEY") 
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
+
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/olinssar/livebot3",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
+GITHUB_REPO = getenv("GITHUB_REPO", "https://github.com/olinssar/livebot3")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
+
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/NNINB")
 SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/NKINB")
-AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "False")
-AUTO_LEAVE_ASSISTANT_TIME = int(getenv("ASSISTANT_LEAVE_TIME", "11500"))
+
+
+AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "True")
+AUTO_LEAVE_ASSISTANT_TIME = int(getenv("ASSISTANT_LEAVE_TIME", "5400"))
 AUTO_SUGGESTION_TIME = int(getenv("AUTO_SUGGESTION_TIME", "5400"))
 AUTO_DOWNLOADS_CLEAR = getenv("AUTO_DOWNLOADS_CLEAR", None)
-AUTO_SUGGESTION_MODE = getenv("AUTO_SUGGESTION_MODE", None)
+AUTO_SUGGESTION_MODE = getenv("AUTO_SUGGESTION_MODE", True)
 PRIVATE_BOT_MODE = getenv("PRIVATE_BOT_MODE", None)
 YOUTUBE_DOWNLOAD_EDIT_SLEEP = int(getenv("YOUTUBE_EDIT_SLEEP", "3"))
 TELEGRAM_DOWNLOAD_EDIT_SLEEP = int(getenv("TELEGRAM_EDIT_SLEEP", "5"))
-GITHUB_REPO = getenv("GITHUB_REPO", None)
+
 SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", None)
 SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", None)
+
 VIDEO_STREAM_LIMIT = int(getenv("VIDEO_STREAM_LIMIT", "3"))
+
 SERVER_PLAYLIST_LIMIT = int(getenv("SERVER_PLAYLIST_LIMIT", "30"))
+
 PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", "25"))
+
 CLEANMODE_DELETE_MINS = int(getenv("CLEANMODE_MINS", "5"))
+
 TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", "104857600"))
+
 TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", "1073741824"))
 # https://www.gbmb.org/mb-to-bytes
 
@@ -52,17 +71,21 @@ STRING5 = getenv("STRING_SESSION5", None)
 BANNED_USERS = filters.user()
 YTDOWNLOADER = 1
 LOG = 2
-LOG_FILE_NAME = "randilogs.txt"
+LOG_FILE_NAME = "logs.txt"
 adminlist = {}
 lyrical = {}
 chatstats = {}
 userstats = {}
 clean = {}
-
 autoclean = []
 
-START_IMG_URL = getenv("START_IMG_URL","https://kindidev.com/live/photo_%D8%A7%D8%B5%D9%81%D8%B1.jpg")
-PING_IMG_URL = getenv("PING_IMG_URL","https://kindidev.com/live/photo_%D8%A7%D8%B5%D9%81%D8%B1.jpg",)
+
+START_IMG_URL = getenv("START_IMG_URL", "https://kindidev.com/live/photo_%D8%A7%D8%B5%D9%81%D8%B1.jpg")
+
+PING_IMG_URL = getenv(
+    "PING_IMG_URL",
+    "https://te.legra.ph/file/159d3f9e2d57dd02db970.jpg",
+)
 
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/6054be6dbfc0f654db62b.jpg"
 
